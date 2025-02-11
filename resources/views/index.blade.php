e34f2ce387b1676477ea0c3ca1a98b9b64966c5b
laDick1337 <vlad.rudenko1337@gmail.com>
2025-02-11 16:22:18 +0300

    seo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="description" content="Доставка еды в Донецке и Макеевке. Заказывайте пиццу, суши, обеды на дом и многое другое с доставкой быстро и удобно!">
+    <meta name="keywords" content="доставка еды Донецк, доставка еды Макеевка, заказать еду на дом Донецк, доставка пиццы Донецк, доставка суши Донецк, доставка обедов Макеевка, еда на дом ДНР, доставка еды быстро Донецк, рестораны с доставкой Донецк, акции доставки еды Донецк, доставка еды Рейв Бургер, доставка еды Рейв, доставка еды Рейв суши, доставка еды Рейв Бистро">
     <link rel="icon" type="image/png" href="/favicon.png">
     <link rel="stylesheet" href="/css/normalize.css">
     <link rel="stylesheet" href="/css/app.css">
