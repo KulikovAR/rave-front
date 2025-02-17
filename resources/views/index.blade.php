4724594a9aaac2939a2b961d02f95c6e332cffb1
laDick1337 <vlad.rudenko1337@gmail.com>
2025-02-17 21:41:26 +0300

    FileVersionsFix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,9 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <!-- <meta name="description" content="Доставка еды в Донецке и Макеевке. Заказывайте пиццу, суши, обеды на дом и многое другое с доставкой быстро и удобно!">
     <meta name="keywords" content="доставка еды Донецк, доставка еды Макеевка, заказать еду на дом Донецк, доставка пиццы Донецк, доставка суши Донецк, доставка обедов Макеевка, еда на дом ДНР, доставка еды быстро Донецк, рестораны с доставкой Донецк, акции доставки еды Донецк, доставка еды Рейв Бургер, доставка еды Рейв, доставка еды Рейв суши, доставка еды Рейв Бистро"> -->
+    
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <link rel="icon" type="image/png" href="/favicon.png">
     <link rel="stylesheet" href="/css/normalize.css">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <link rel="stylesheet" href="/css/variables.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,21 +1,18 @@
+const mix = require('laravel-mix');
 const webpack = require('webpack');
-const mix = require("laravel-mix");
 
 mix.js('resources/js/app.js', 'public/js')
-    .vue() // Compile Vue files
+    .vue() // Компиляция Vue файлов
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('autoprefixer'),
     ])
-    .version()
     .postCss('resources/css/variables.css', 'public/css')
+    .version() // Это заставит Laravel Mix генерировать уникальные имена файлов для кеширования
     .webpackConfig({
         plugins: [
             new webpack.DefinePlugin({
                 'process.env.MIX_API_URL': JSON.stringify(process.env.MIX_API_URL)
             })
-        ],
-        // devServer: {
-        //     historyApiFallback: true, // Добавляем эту строку
-        // }
+        ]
     });
